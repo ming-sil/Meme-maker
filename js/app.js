5c44bde361a021cc6c152d3b58216d7875b373a9
ming-sil <106130309+ming-sil@users.noreply.github.com>
2022-09-09 23:32:38 +0900

    Paint Color 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
@@ -37,6 +40,13 @@ const onColorChange = (e) => {
   ctx.fillStyle = e.target.value;
 };
 
+const onColorClick = (e) => {
+  const colorValue = e.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+};
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -44,3 +54,5 @@ canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
